3b565fcba086235a39772538f2447581ae8f3ecb
alex8yashin8base <alex8yashin8base@gmail.com>
2023-11-28 17:19:05 +0700

    docker-compose create

--- notes.py
+++ notes.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import json
 import os
 from typing import Dict, Any
@@ -19,7 +19,7 @@ def write_data(data: Dict[str, Any]) -> None:
 @app.route('/notes', methods=['GET'])
 def get_notes() -> Any:
     data = read_data()
-    return jsonify(data), 200
+    return make_response(jsonify(data), 200)
 
 @app.route('/notes/<key>', methods=['GET'])
 def get_note(key: str) -> Any:
@@ -27,7 +27,7 @@ def get_note(key: str) -> Any:
     note = data.get(key)
     if note:
         return jsonify({key: note})
-    return jsonify({"error": "Note not found"}), 404
+    return make_response(jsonify({"error": "Note not found"}), 404)
 
 @app.route('/notes', methods=['POST'])
 def add_note() -> Any:
@@ -36,21 +36,21 @@ def add_note() -> Any:
     for key, value in note_data.items():
         data[key] = value
     write_data(data)
-    return jsonify({"message": "Data updated"}), 200
+    return make_response(jsonify({"message": "Data updated"}), 200)
 
 @app.route('/notes/<key>', methods=['DELETE'])
 def delete_note(key: str) -> Any:
     data = read_data()
     if key not in data:
-        return jsonify({"error": "Note not found"}), 404
+        return make_response(jsonify({"error": "Note not found"}), 404)
     data.pop(key)
     write_data(data)
-    return jsonify({"message": "Note deleted"}), 200
+    return make_response(jsonify({"message": "Note deleted"}), 200)
 
 @app.route('/notes', methods=['DELETE'])
 def delete_all_notes() -> Any:
     write_data({})
-    return jsonify({"message": "All notes deleted"}), 200
+    return make_response(jsonify({"message": "All notes deleted"}), 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
