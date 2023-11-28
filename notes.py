d42747037814a013c1f62404cee1312ba658ed31
alex8yashin8base <alex8yashin8base@gmail.com>
2023-11-28 16:25:49 +0700

    Type annotations

--- notes.py
+++ notes.py
@@ -1,27 +1,28 @@
 from flask import Flask, jsonify, request
 import json
 import os
+from typing import Dict, Any
 
 app = Flask(__name__)
-data_file = 'data.txt'
+data_file: str = 'data.txt'
 
-def read_data():
+def read_data() -> Dict[str, Any]:
     if not os.path.exists(data_file):
         return {}
     with open(data_file, 'r') as file:
         return json.load(file)
 
-def write_data(data):
+def write_data(data: Dict[str, Any]) -> None:
     with open(data_file, 'w') as file:
         json.dump(data, file)
 
 @app.route('/notes', methods=['GET'])
-def get_notes():
+def get_notes() -> Any:
     data = read_data()
     return jsonify(data)
 
 @app.route('/notes/<key>', methods=['GET'])
-def get_note(key):
+def get_note(key: str) -> Any:
     data = read_data()
     note = data.get(key)
     if note:
@@ -29,7 +30,7 @@ def get_note(key):
     return jsonify({"error": "Note not found"})
 
 @app.route('/notes', methods=['POST'])
-def add_note():
+def add_note() -> Any:
     data = read_data()
     note_data = request.json
     for key, value in note_data.items():
@@ -38,7 +39,7 @@ def add_note():
     return jsonify({"message": "Data updated"})
 
 @app.route('/notes/<key>', methods=['DELETE'])
-def delete_note(key):
+def delete_note(key: str) -> Any:
     data = read_data()
     if key not in data:
         return jsonify({"error": "Note not found"})
@@ -47,7 +48,7 @@ def delete_note(key):
     return jsonify({"message": "Note deleted"})
 
 @app.route('/notes', methods=['DELETE'])
-def delete_all_notes():
+def delete_all_notes() -> Any:
     write_data({})
     return jsonify({"message": "All notes deleted"})
 
